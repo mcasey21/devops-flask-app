bf9fc280b450aacc60180a680091c5cc0225f0be
mcasey21 <casey.michael1231@gmail,com>
2025-09-28 13:07:01 +0000

    changes to flask app

--- hello.py
+++ hello.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<p>Hello World, I am a Flash app!</p> <a href="/about">Click here</a>'
+	return '<p>Hello, This is my Flask app!</p> <a href="/about">About Page</a> <a href="/contact">Contact Page</a>'
 
 @app.route('/about')
 def about_page():
 	return '<p>This is the about page</p>'
+
+@app.route('/contact')
+def contact_page():
+	return '<p>My Email: C23343691@mytudublin.ie'
